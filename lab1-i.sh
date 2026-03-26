76659d5f7f3737a2112701e1c877f191593cacf2
Enosh S Borges <enosh.s.borges@gmail.com>
2026-03-26 20:24:42 +0530

    Lab1b-i: Concatenation of two strings

--- lab1-i.sh
+++ lab1-i.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+#Write a shell script for the concatenation of two strings
+
 echo "Enter string-1: "
 read -r str1
 echo "Enter string-2: "
 read -r str2
 echo "The concatenated message is $str1$str2"
-
