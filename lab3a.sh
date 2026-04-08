cd47bba350df9b40ef0801f60384eb112207a595
Enosh S Borges <enosh.s.borges@gmail.com>
2026-04-08 19:42:33 +0530

    Lab3 session added

---
+++ lab3a.sh
@@ -0,0 +1,17 @@
+# Write a shell script for finding the largest of N numbers (storing the numbers in an array)
+
+#!/bin/bash
+echo "Enter the number of elements: "
+read -r n
+declare -a arr
+echo "Enter the numbers: "
+for ((i = 0; i < n; i++)); do
+    read -r arr[i]
+done
+largest=${arr[0]}
+for ((i = 1; i < n; i++)); do
+    if [ "${arr[i]}" -gt "$largest" ]; then
+        largest=${arr[i]}
+    fi
+done
+echo "The largest number is: $largest"
