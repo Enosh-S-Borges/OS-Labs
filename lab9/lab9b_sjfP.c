41298cee54497f2bf22bcee5e3326d44612f84cd
Enosh S Borges <enosh.s.borges@gmail.com>
2026-05-21 19:03:45 +0530

    typo corrected lab9b

--- lab9/lab9b_sjfP.c
+++ lab9/lab9b_sjfP.c
@@ -6,7 +6,7 @@ b. SJF (Preemptive)
 
 //b) SJF (Preemptive)
 
-#include<stdio.h>
+#include <stdio.h>
 
 typedef struct process {
     int processId;
@@ -46,19 +46,25 @@ int main() {
 
 void sjfPreemptive(Process processes[], int n) {
     int timeProgress[100], processTrack[100], j = -1, k = -1;
-    float avgWaitingTime = 0, avgTurnAroundTime = 0, avgResponseTime = 0;
-    int totalWaitingTime = 0, totalTurnAroundTime = 0, totalResponseTime = 0;
+    float avgWaitingTime = 0;
+    float avgTurnAroundTime = 0;
+    float avgResponseTime = 0;
+    int totalWaitingTime = 0;
+    int totalTurnAroundTime = 0;
+    int totalResponseTime = 0;
     int elapsedTime = 0;
     int remainingProcesses = n;
 
     timeProgress[++j] = 0;
 
-    while (remainingProcesses) {
+    while (remainingProcesses > 0) {
         int exec = -1;
         int shortestBurstTime = 9999;
 
+        // Find process with shortest remaining time that has arrived
         for (int i = 0; i < n; i++) {
-            if (processes[i].arrivalTime <= elapsedTime && processes[i].remainingTime > 0) {
+            if (processes[i].arrivalTime <= elapsedTime && 
+                processes[i].remainingTime > 0) {
                 if (processes[i].remainingTime < shortestBurstTime) {
                     shortestBurstTime = processes[i].remainingTime;
                     exec = i;
@@ -67,23 +73,26 @@ void sjfPreemptive(Process processes[], int n) {
         }
 
         if (exec == -1) {
+            // Idle time
             processTrack[++k] = 0;
             elapsedTime++;
             timeProgress[++j] = elapsedTime;
             continue;
         }
 
-        // First time CPU allocation
+        // Record response time for first execution
         if (processes[exec].remainingTime == processes[exec].burstTime) {
             processes[exec].responseTime = elapsedTime - processes[exec].arrivalTime;
         }
 
+        // Execute for 1 unit time
         processes[exec].remainingTime--;
         elapsedTime++;
 
         processTrack[++k] = processes[exec].processId;
         timeProgress[++j] = elapsedTime;
 
+        // If process completed
         if (processes[exec].remainingTime == 0) {
             processes[exec].completionTime = elapsedTime;
             processes[exec].turnAroundTime = processes[exec].completionTime - processes[exec].arrivalTime;
@@ -96,20 +105,21 @@ void sjfPreemptive(Process processes[], int n) {
         }
     }
 
-    avgTurnAroundTime = (float)totalTurnAroundTime / n;
-    avgWaitingTime = (float)totalWaitingTime / n;
-    avgResponseTime = (float)totalResponseTime / n;
-
-    printf("\nObservation Table\nPID\tAT\tBT\tCT\tTT\tWT\tRT\n");
-
+    // Print Observation Table
+    printf("\nObservation Table\n");
+    printf("PID\tAT\tBT\tCT\tTT\tWT\tRT\n");
     for (int i = 0; i < n; i++) {
-        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n", 
-               processes[i].processId, processes[i].arrivalTime, 
-               processes[i].burstTime, processes[i].completionTime, 
-               processes[i].turnAroundTime, processes[i].waitingTime, 
+        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
+               processes[i].processId,
+               processes[i].arrivalTime,
+               processes[i].burstTime,
+               processes[i].completionTime,
+               processes[i].turnAroundTime,
+               processes[i].waitingTime,
                processes[i].responseTime);
     }
 
+    // Print Gantt Chart
     printf("\nGantt Chart\n(P0 --> idle time)\n");
     for (int i = 0; i <= k; i++) {
         printf("| P%d\t", processTrack[i]);
@@ -120,6 +130,11 @@ void sjfPreemptive(Process processes[], int n) {
         printf("%d\t", timeProgress[i]);
     }
     printf("\n");
+
+    // Print Averages
+    avgTurnAroundTime = (float)totalTurnAroundTime / n;
+    avgWaitingTime = (float)totalWaitingTime / n;
+    avgResponseTime = (float)totalResponseTime / n;
 
     printf("\nAverage Waiting Time: %.2f\n", avgWaitingTime);
     printf("Average Turnaround Time: %.2f\n", avgTurnAroundTime);
